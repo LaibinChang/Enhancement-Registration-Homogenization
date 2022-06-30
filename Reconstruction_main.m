6b149739e597e714f464cbc66f7d58b98acb6ed1
LaibinChang <88143736+LaibinChang@users.noreply.github.com>
2022-06-30 15:00:43 +0800

    Add files via upload

---
+++ Reconstruction_main.m
@@ -0,0 +1,13 @@
+%% Before running this Reconstruction_main.m, you should copy the enhanced results to the corresponding folder, which
+%% are based on the number of your images.
+%% Reconstration_with_two_images/three_images/four_images/five_images/six_images.
+close all;
+clear all;
+clc;
+%cd 
+ExeFileName= 'ERH.exe';
+ExeFilePath= fullfile(ExeFileName);
+%name:你的文件夹名
+param1=[' ','Reconstration_with_two_images'];%Modify it according to your folder name.
+Cmd = [ExeFilePath,param1]
+system(Cmd);
